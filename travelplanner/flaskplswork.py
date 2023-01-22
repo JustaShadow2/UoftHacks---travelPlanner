c85ec3f36737ebf6cd70340a619e77cbd95cde9a
Arjun <109932685+JustaShadow2@users.noreply.github.com>
2023-01-21 22:14:40 -0500

    dsda

--- travelplanner/flaskplswork.py
+++ travelplanner/flaskplswork.py
@@ -3,8 +3,7 @@ app = Flask(__name__)
 
 @app.route('/reccomendations/<place>')
 def reccomendations(place):
-#    return 'welcome %s' % place
-    import requests
+    import requests 
     import json
 
     api_key = "jvFKqmh_l2XRzDI1I9hXuGq0RntzZUhXvuk2lxs9gLFXPaC80uEDBlMyTsHoJYa6aOhDPQDEI8hNUy8COdJdB8gvFv8TIpTGy0teJ5q47SC6643BuQlXRB6_4BzMY3Yx"
@@ -20,12 +19,16 @@ def reccomendations(place):
 
     return{'name': parsed['businesses'][0]['name'], 'rating': parsed['businesses'][0]['rating'], 'name2': parsed['businesses'][1]['name'], 'rating2': parsed['businesses'][1]['rating'], 'name3': parsed['businesses'][2]['name'], 'rating3': parsed['businesses'][2]['rating']}
 
+@app.route('/map/<place>/')
+def map(place):
+    return 'Map of ' + place
 
 @app.route('/index',methods = ['POST', 'GET'])
 def login():
    if request.method == 'POST':
-      dest = request.form['endLocation']
-      return redirect(url_for('reccomendations',place = dest))
+      dest = request.form['dest']
+      home = request.form['home']
+      return redirect(url_for('map',place = dest, place2 = home))
 #    else:
 #       dest = request.args.get('nm')
 #       return redirect(url_for('success',place = dest))
