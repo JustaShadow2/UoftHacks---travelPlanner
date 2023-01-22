283a8d9adf7c62271e5e04329a6205a127d1a26c
Arjun <109932685+JustaShadow2@users.noreply.github.com>
2023-01-22 06:50:28 -0500

    jfdasl

--- travelplanner/flaskplswork.py
+++ travelplanner/flaskplswork.py
@@ -17,38 +17,41 @@ def reccomendations(place):
     parsed = json.loads(req.text)
     data = parsed['businesses'][0]['name'], parsed['businesses'][0]['rating'], parsed['businesses'][1]['name'], parsed['businesses'][1]['rating'], parsed['businesses'][2]['name'], parsed['businesses'][2]['rating']
     #send data to console log
-    return render_template('reccomendations.html', data=data)
-
-
-@app.route('/map/<place>/')
-def map(place):
-    return render_template('mapData.html', place=place)
-
-@app.route('/schedule/<place>')
-def schedule(place):
-    api_key = "jvFKqmh_l2XRzDI1I9hXuGq0RntzZUhXvuk2lxs9gLFXPaC80uEDBlMyTsHoJYa6aOhDPQDEI8hNUy8COdJdB8gvFv8TIpTGy0teJ5q47SC6643BuQlXRB6_4BzMY3Yx"
-    headers = {'Authorization' : 'Bearer {}'.format(api_key)}
-    destination = place
-    url = 'https://api.yelp.com/v3/businesses/search'
-
-    params = {'term':'food', 'location':destination, 'limit':3, 'sort_by':'rating'}
-
-    req = requests.get(url, params=params, headers=headers)
-
-    parsed = json.loads(req.text)
-    data = parsed['businesses'][0]['name'], parsed['businesses'][0]['rating'], parsed['businesses'][1]['name'], parsed['businesses'][1]['rating'], parsed['businesses'][2]['name'], parsed['businesses'][2]['rating']    
-    
     import openai
-
-    openai.api_key = ("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")
+    openai.api_key = ("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")
     response = openai.Completion.create(
         model="text-davinci-003",
         prompt=("Give me a 3 day schedule for a trip to {} including {}, {} and {}".format(destination, data[0], data[2], data[4])),
         max_tokens=500,
         temperature=0,)
-
     itinerary = (str(response['choices'][0]['text']))
-    return render_template('schedule.html', itinerary=itinerary)
+    return render_template('reccomendations.html', data=data, itinerary=itinerary)
+
+
+# @app.route('/map/<place>/')
+# def map(place):
+#     return render_template('wheredoigo.html', place=place)
+
+# @app.route('/schedule/<place>')
+# def schedule(place):
+#     api_key = "jvFKqmh_l2XRzDI1I9hXuGq0RntzZUhXvuk2lxs9gLFXPaC80uEDBlMyTsHoJYa6aOhDPQDEI8hNUy8COdJdB8gvFv8TIpTGy0teJ5q47SC6643BuQlXRB6_4BzMY3Yx"
+#     headers = {'Authorization' : 'Bearer {}'.format(api_key)}
+#     destination = place
+#     url = 'https://api.yelp.com/v3/businesses/search'
+#     params = {'term':'food', 'location':destination, 'limit':3, 'sort_by':'rating'}
+#     req = requests.get(url, params=params, headers=headers)
+#     parsed = json.loads(req.text)
+#     data = parsed['businesses'][0]['name'], parsed['businesses'][0]['rating'], parsed['businesses'][1]['name'], parsed['businesses'][1]['rating'], parsed['businesses'][2]['name'], parsed['businesses'][2]['rating']    
+    
+#     import openai
+#     openai.api_key = ("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")
+#     response = openai.Completion.create(
+#         model="text-davinci-003",
+#         prompt=("Give me a 3 day schedule for a trip to {} including {}, {} and {}".format(destination, data[0], data[2], data[4])),
+#         max_tokens=500,
+#         temperature=0,)
+#     itinerary = (str(response['choices'][0]['text']))
+#     return render_template('schedule.html', itinerary=itinerary)
 
 
 @app.route('/index',methods = ['POST', 'GET'])
@@ -56,7 +59,8 @@ def login():
    if request.method == 'POST':
       dest = request.form['dest']
       home = request.form['home']
-      return redirect(url_for('map',place = dest, place2 = home))
+      #send dest to other page
+      return redirect(url_for('reccomendations',place = dest, place2 = home))
 
 
 if __name__ == '__main__':
